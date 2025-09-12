e8a18f014db1d8900f15eb925058919bbe328ff3
jason-zhao-git <zjq233333@gmail.com>
2025-09-11 21:41:25 -0400

    added soma download

---
+++ hpc_workaround/download_to_local/download_soma.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# This script downloads the CELLxGENE Census SOMA data for a specific release.
+# It uses wget to recursively download the directory from S3.
+# NOTE: The recommended tool for this is 'aws s3 sync'. This script is a workaround.
+
+RELEASE_TAG="2025-01-30"
+BASE_URL="https://cellxgene-census-public-us-west-2.s3.us-west-2.amazonaws.com"
+DATA_PATH="cell-census/${RELEASE_TAG}/soma"
+OUTPUT_DIR="/scratch/sigbio_project_root/sigbio_project25/jingqiao/mccell-single/soma_db"
+
+echo "Downloading SOMA data for release ${RELEASE_TAG}..."
+echo "Output directory: ${OUTPUT_DIR}"
+
+# -r: recursive
+# -np: no-parent (don't go to parent directories)
+# -nH: no-host-directories (don't create a directory named after the host)
+# --cut-dirs=3: remove the first 3 directories from the path (cell-census, ${RELEASE_TAG}, soma)
+# -R "index.html*": reject index.html files
+# --directory-prefix: download files into this directory
+wget -r -np -nH --cut-dirs=3 -R "index.html*" --directory-prefix=${OUTPUT_DIR} ${BASE_URL}/${DATA_PATH}/
+
+echo "Download complete."
